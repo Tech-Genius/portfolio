4accfc188939ba0d0c7f39c3d0491ecdebb4b490
Tech-Genius <adebimpehabeeb10@gmail.com>
2022-11-02 10:26:41 +0100

    try rehosting

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,7 +134,6 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
