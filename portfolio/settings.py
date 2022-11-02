837419a1f2a5068af566d2e4b415453a31eccbd2
Tech-Genius <adebimpehabeeb10@gmail.com>
2022-11-02 11:22:03 +0100

    add hand icon to homepage

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,7 +134,10 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'adebimpehabeeb8@gmail.com'
+EMAIL_HOST_PASSWORD = 'rmiyfdprziisvatt'
