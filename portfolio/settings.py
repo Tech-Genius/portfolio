68b2316357babe4c13f41e75f71e5b38d0afa97a
Tech-Genius <adebimpehabeeb10@gmail.com>
2022-12-01 14:13:23 +0100

    remove mail details

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,8 +135,6 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_HOST_USER = 'adebimpehabeeb8@gmail.com'
-EMAIL_HOST_PASSWORD = 'rmiyfdprziisvatt'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
