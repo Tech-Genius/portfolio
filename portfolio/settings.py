f1914d5a2cef836369e718cdc6cad677fe549b99
Tech-Genius <adebimpehabeeb10@gmail.com>
2022-12-29 13:03:56 +0100

    colors changed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,7 +134,8 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+EMAIL_HOST_USER = 'adebimpehabeeb8@gmail.com'
+EMAIL_HOST_PASSWORD = 'rmiyfdprziisvatt'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
